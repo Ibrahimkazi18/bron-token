5fd8b336da0763fb6e229a62fc5da84152e48050
Ibrahim <ibirfkazi@gmail.com>
2025-07-12 14:51:49 +0530

    airdrop address

--- app/airdrop/page.tsx
+++ app/airdrop/page.tsx
@@ -253,7 +253,7 @@ function LoadingScreen() {
 
   return (
     <div className="text-center justify-center flex flex-col w-full min-h-screen bg-black text-white">
-      <p className="text-3xl mb-2">Loading your game...</p>
+      <p className="text-3xl mb-2">Loading your airdrop game...</p>
       <p className="text-3xl font-bold">{progress.toFixed(0)}%</p>
     </div>
   )
